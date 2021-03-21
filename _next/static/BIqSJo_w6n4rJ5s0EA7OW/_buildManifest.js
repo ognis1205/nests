84b7785deb95290dd10e5ec42757e41d4e8539a6
ognis1205 <ognis1205@users.noreply.github.com>
2021-03-21 18:11:15 +0000

    deploy: b8d211a7dfff079d56aa4666e4d6011748ed4798

--- _next/static/BIqSJo_w6n4rJ5s0EA7OW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F5dfbb5c3dde5275c2c42.css","static\u002Fchunks\u002Fpages\u002Findex-8c6e108a239d5271bc75.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a71b8ec1c94201de3df2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
