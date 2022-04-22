62ee908f1cedc3e32f8e7d7876c1c855183b6408
ognis1205 <ognis1205@users.noreply.github.com>
2022-04-22 22:47:14 +0000

    deploy: a354988a8728ff7005f9ffc3dc076659b7553a6e

---
+++ _next/static/YFrWaqnJxalCGMy-PFVIy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5346d770527de331.css","static/chunks/pages/index-b418a0b15e77c890.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
